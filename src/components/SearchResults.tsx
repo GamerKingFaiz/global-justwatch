f96351d4167eb2eb11b12c37acd21411db459a6c
GamerKingFaiz <gamerkingfaiz@gmail.com>
2022-09-28 21:09:00 -0700

    Search results appear on top of the page

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -22,45 +22,61 @@ interface Props {
 }
 
 const SearchResults = ({ loading, searchResults, setOpen }: Props) => {
-  return loading ? (
-    <Skeleton variant='rounded' width={SEARCH_WIDTH} height={SEARCH_HEIGHT} />
-  ) : (
-    <Box zIndex={2}>
-      {searchResults.map((result: GenericObject, index) => {
-        return (
-          <StyledLink
-            to={`/media/${result.object_type}/${result.id}`}
-            key={index}
-            onClick={() => setOpen(false)}
-          >
-            <Card
-              sx={{
-                mb: 1,
-                width: SEARCH_WIDTH,
-                height: SEARCH_HEIGHT,
-                display: 'flex',
-              }}
+  return (
+    <Box
+      zIndex={2}
+      position='absolute'
+      top={80}
+      bgcolor={grey[900]}
+      width={SEARCH_WIDTH}
+      display='flex'
+      flexDirection='column'
+      alignItems='center'
+      borderRadius='4px'
+    >
+      {loading ? (
+        <Skeleton
+          variant='rounded'
+          width={SEARCH_WIDTH}
+          height={SEARCH_HEIGHT}
+        />
+      ) : (
+        searchResults.map((result: GenericObject, index) => {
+          return (
+            <StyledLink
+              to={`/media/${result.object_type}/${result.id}`}
+              key={index}
+              onClick={() => setOpen(false)}
             >
-              {result.poster && (
-                <CardMedia
-                  sx={{ width: 53 }}
-                  component='img'
-                  image={`https://images.justwatch.com/poster/${stripLetters(
-                    result.poster
-                  )}/s592`}
-                  alt='media poster'
-                />
-              )}
-              <CardContent>
-                <Typography>{result.title}</Typography>
-                <Typography variant='caption' color={grey[600]}>
-                  {result.object_type}, {result.original_release_year}
-                </Typography>
-              </CardContent>
-            </Card>
-          </StyledLink>
-        );
-      })}
+              <Card
+                sx={{
+                  mb: index === 3 ? 0 : 1,
+                  width: SEARCH_WIDTH,
+                  height: SEARCH_HEIGHT,
+                  display: 'flex',
+                }}
+              >
+                {result.poster && (
+                  <CardMedia
+                    sx={{ width: 53 }}
+                    component='img'
+                    image={`https://images.justwatch.com/poster/${stripLetters(
+                      result.poster
+                    )}/s592`}
+                    alt='media poster'
+                  />
+                )}
+                <CardContent>
+                  <Typography>{result.title}</Typography>
+                  <Typography variant='caption' color={grey[600]}>
+                    {result.object_type}, {result.original_release_year}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </StyledLink>
+          );
+        })
+      )}
     </Box>
   );
 };

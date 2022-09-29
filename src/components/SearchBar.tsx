628d282349657ca8369d2febc5fb178c4aa4f407
GamerKingFaiz <gamerkingfaiz@gmail.com>
2022-09-28 20:25:42 -0700

    Fixed search bar bugs

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { Box, ClickAwayListener, TextField } from '@mui/material';
+import { Box, TextField } from '@mui/material';
 import { useState } from 'react';
 import { GenericObject, SEARCH_WIDTH } from '../utils/constants';
 import SearchResults from './SearchResults';
@@ -10,50 +10,47 @@ interface Props {
 }
 
 const SearchBox = ({ setSearchInput, loading, searchResults }: Props) => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
   return (
     <Box display='flex' justifyContent='center'>
-      <ClickAwayListener onClickAway={() => setOpen(false)}>
-        <Box
-          m={2}
-          display='flex'
-          flexDirection='column'
-          alignItems='center'
-          width='100%'
-          maxWidth={SEARCH_WIDTH}
-        >
-          <TextField
-            sx={{ width: '100%', mb: 1, zIndex: 2 }}
-            id='gjwSearchBar'
-            label='Search for media...'
-            variant='filled'
-            autoFocus
-            autoComplete='off'
-            onFocus={() => setOpen(true)}
-            onChange={(e) => setSearchInput(e.target.value)}
+      <Box
+        m={2}
+        display='flex'
+        flexDirection='column'
+        alignItems='center'
+        width='100%'
+        maxWidth={SEARCH_WIDTH}
+      >
+        <TextField
+          sx={{ width: '100%', mb: 1, zIndex: 2 }}
+          id='gjwSearchBar'
+          label='Search for media...'
+          variant='filled'
+          autoComplete='off'
+          onFocus={() => setOpen(true)}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        {open && (
+          <SearchResults
+            loading={loading}
+            searchResults={searchResults}
+            setOpen={setOpen}
           />
-          {open && (
-            <SearchResults
-              loading={loading}
-              searchResults={searchResults}
-              setOpen={setOpen}
-            />
-          )}
-          {/* Click blocker */}
-          {open && (
-            <Box
-              position='fixed'
-              width='100%'
-              height='100%'
-              top={0}
-              left={0}
-              bgcolor='rgba(0,0,0,0.5)'
-              onClick={() => setOpen(false)}
-            ></Box>
-          )}
-        </Box>
-      </ClickAwayListener>
+        )}
+        {/* Click blocker */}
+        {open && searchResults.length > 0 && (
+          <Box
+            position='fixed'
+            width='100%'
+            height='100%'
+            top={0}
+            left={0}
+            bgcolor='rgba(0,0,0,0.5)'
+            onClick={() => setOpen(false)}
+          ></Box>
+        )}
+      </Box>
     </Box>
   );
 };
